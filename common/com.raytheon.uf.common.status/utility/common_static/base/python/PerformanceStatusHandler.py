398c92f082e2dc1d3901510097634fd4b9a17e7b
Ron Anderson <ron.anderson@raytheon.com>
2021-12-14 13:50:13 -0600

    Omaha #8342 Add Python PerformanceStatusHandler
    - Also updated Python UFStatusHandler to use logging constants instead
      of hard coded literals for priority translation
    
    Change-Id: I6a8d0744fa2b2942e116e40507d0d55cdebacbec

---
+++ common/com.raytheon.uf.common.status/utility/common_static/base/python/PerformanceStatusHandler.py
@@ -0,0 +1,70 @@
+##
+# This software was developed and / or modified by Raytheon Company,
+# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+#
+# U.S. EXPORT CONTROLLED TECHNICAL DATA
+# This software product contains export-restricted data whose
+# export/transfer/disclosure is restricted by U.S. law. Dissemination
+# to non-U.S. persons whether in the United States or abroad requires
+# an export license or other authorization.
+#
+# Contractor Name:        Raytheon Company
+# Contractor Address:     6825 Pine Street, Suite 340
+#                         Mail Stop B8
+#                         Omaha, NE 68106
+#                         402.291.0100
+#
+# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+# further licensing information.
+##
+
+from com.raytheon.uf.common.status import PerformanceStatus
+
+#
+# Python performance logging mechanism to allow python to log to the
+# *_perf.log file using the same format as Java code.
+#
+# Example usage:
+#    perfLog = PerformanceStatusHandler("MyFunction:")
+#    t0 = time.perf_counter()
+#
+#    # perform action to be timed
+#
+#    t1 = time.perf_counter()
+#    perfLog.logDuration("description of action", t1-t0)
+#
+# Sample performance log output:
+#   INFO  2021-12-10 11:41:09,174 1318 [thread-id] PerformanceLogger: MyFunction: description of action took nnn ms
+#
+# SOFTWARE HISTORY
+#
+# Date          Ticket#  Engineer  Description
+# ------------- -------- --------- --------------------------------------------
+# Dec 10, 2021  8342     randerso  Initial Creation.
+#
+##
+
+
+class PerformanceStatusHandler():
+
+    def __init__(self, prefix):
+        self._perfLog = PerformanceStatus.getHandler(prefix)
+
+    def log(self, message):
+        """
+        Log a message to the performance log
+
+        Args:
+            message:    the message to be logged
+        """
+        self._perfLog.log(str(message))
+
+    def logDuration(self, message, duration):
+        """
+        Log a message to the performance log with duration
+
+        Args:
+            message:    the message to be logged
+            duration:   duration in seconds
+        """
+        self._perfLog.logDuration(str(message), int(duration * 1000))
--- common/com.raytheon.uf.common.status/utility/common_static/base/python/UFStatusHandler.py
+++ common/com.raytheon.uf.common.status/utility/common_static/base/python/UFStatusHandler.py
@@ -1,19 +1,19 @@
 ##
 # This software was developed and / or modified by Raytheon Company,
 # pursuant to Contract DG133W-05-CQ-1067 with the US Government.
-# 
+#
 # U.S. EXPORT CONTROLLED TECHNICAL DATA
 # This software product contains export-restricted data whose
 # export/transfer/disclosure is restricted by U.S. law. Dissemination
 # to non-U.S. persons whether in the United States or abroad requires
 # an export license or other authorization.
-# 
+#
 # Contractor Name:        Raytheon Company
 # Contractor Address:     6825 Pine Street, Suite 340
 #                         Mail Stop B8
 #                         Omaha, NE 68106
 #                         402.291.0100
-# 
+#
 # See the AWIPS II Master Rights File ("Master Rights File.pdf") for
 # further licensing information.
 ##
@@ -25,24 +25,25 @@ import logging
 #
 # Python logging mechanism for logging through Java to UFStatus.
 # When using this class the python logger level should not be set.
-# This will allow the filtering to be handled by the UFStatus. 
-#    
-#     SOFTWARE HISTORY
-#    
-#    Date            Ticket#       Engineer       Description
-#    ------------    ----------    -----------    --------------------------
-#    07/19/09                      njensen       Initial Creation.
-#    02/13/2015      4038          rferrel        Force root logging to NOTSET
-#    Apr 25, 2015    4952          njensen        Updated for new JEP API
-#    
-# 
+# This will allow the filtering to be handled by the UFStatus.
 #
+# SOFTWARE HISTORY
+#
+# Date          Ticket#  Engineer  Description
+# ------------- -------- --------- --------------------------------------------
+# Jul 19, 2009           njensen   Initial Creation.
+# Feb 13, 2015  4038     rferrel   Force root logging to NOTSET
+# Apr 25, 2015  4952     njensen   Updated for new JEP API
+# Dec 10, 2021  8342     randerso  Updated to use logging constants
+#                                  Made logging.DEBUG map to Priority.VERBOSE
+#
+##
 
-# Force root logging to lowest level NOTSET (0) in order to allow the emit 
+# Force root logging to lowest level NOTSET (0) in order to allow the emit
 # to determine what should be passed to the IUFStatusHandler.
 logging.getLogger().setLevel(logging.NOTSET)
 
-    
+
 class UFStatusHandler(logging.Handler):
 
     def __init__(self, pluginName, category, source=None, level=logging.NOTSET):
@@ -51,24 +52,23 @@ class UFStatusHandler(logging.Handler):
         self._category = category
         self._source = source
         self._handler = UFStatus.getHandler(self._pluginName, self._source)
-        
-    
-    def emit(self, record):        
+
+    def emit(self, record):
         "Implements logging.Handler's interface.  Record argument is a logging.LogRecord."
         priority = None
-        if record.levelno >= 50:
+        if record.levelno >= logging.CRITICAL:
             priority = Priority.CRITICAL
-        elif record.levelno >= 40:
+        elif record.levelno >= logging.ERROR:
             priority = Priority.SIGNIFICANT
-        elif record.levelno >= 30:
+        elif record.levelno >= logging.WARNING:
             priority = Priority.PROBLEM
-        elif record.levelno >= 20:
+        elif record.levelno >= logging.INFO:
             priority = Priority.EVENTA
-        elif record.levelno >= 10:
+        elif record.levelno > logging.DEBUG:
             priority = Priority.EVENTB
         else:
             priority = Priority.VERBOSE
-        
-        if self._handler.isPriorityEnabled(priority) :
+
+        if self._handler.isPriorityEnabled(priority):
             msg = self.format(record)
             self._handler.handle(priority, msg)
