66b628e7dc113027a2300f1aeb84408c68b7a8f4
Lisa Singh <lisa.e.singh@raytheon.com>
2022-09-20 11:43:36 -0400

    Omaha #8341 - Updated Thrift env variable log messages.
    - Updated log messages so that they don't appear in AlertViz as warnings
    - Cleaned up the Thrift log messages so less log entries appear.
    
    Change-Id: I5b92a31e617ede5722b042bd41b4278830879c63

--- common/com.raytheon.uf.common.serialization/src/com/raytheon/uf/common/serialization/thrift/ThriftSerializationContextBuilder.java
+++ common/com.raytheon.uf.common.serialization/src/com/raytheon/uf/common/serialization/thrift/ThriftSerializationContextBuilder.java
@@ -51,6 +51,8 @@ import com.raytheon.uf.common.status.UFStatus;
  * May 27, 2021  8470     lsingh    Upgraded to Thrift 0.14.1. Added exception
  *                                  handling and TConfiguration support.
  * Jan 11, 2022  8341     randerso  Changed to use UFStatus
+ * Sep 20, 2022  8341     lsingh    Changed thrift env var logs to INFO.
+ *                                  Cleaned up log messages.
  *
  * </pre>
  *
@@ -78,89 +80,108 @@ public class ThriftSerializationContextBuilder
         int maxFrameSize = 0;
         int recursionDepth = 0;
 
+        // These values are set in /etc/profile.d/awips2Thrift.sh or
+        // awips2Thrift.csh
         String maxMessageSizeStr = System.getenv("THRIFT_MAX_MESSAGE_SIZE");
         String maxFrameSizeStr = System.getenv("THRIFT_MAX_FRAME_SIZE");
         String recursionDepthStr = System.getenv("THRIFT_RECURSION_DEPTH");
 
         // Parse MAX_MESSAGE_SIZE
-        if (maxMessageSizeStr == null) {
-            log.warn("THRIFT_MAX_MESSAGE_SIZE environmental variable not set.");
-        } else {
+        /*
+         * If MAX_MESSAGE_SIZE received is set too low, then Thrift will fail
+         * during large transfers with the error: MaxMessageSize reached.
+         */
+        if (maxMessageSizeStr != null) {
             try {
                 maxMessageSize = Integer.parseInt(maxMessageSizeStr);
+
+                if (maxMessageSize > 0) {
+                    log.info("THRIFT_MAX_MESSAGE_SIZE is set to "
+                            + maxMessageSize + " bytes");
+                } else {
+                    log.info(
+                            "THRIFT_MAX_MESSAGE_SIZE is not set or set to an invalid value. Setting THRIFT_MAX_MESSAGE_SIZE to default value of "
+                                    + TConfiguration.DEFAULT_MAX_MESSAGE_SIZE
+                                    + " bytes.");
+                    maxMessageSize = TConfiguration.DEFAULT_MAX_MESSAGE_SIZE;
+                }
+
             } catch (NumberFormatException e) {
-                log.warn(
+                log.info(
                         "Could not parse value from THRIFT_MAX_MESSAGE_SIZE environmental variable. Value received was "
-                                + maxMessageSizeStr + ". "
-                                + "Setting THRIFT_MAX_MESSAGE_SIZE to the default value of "
+                                + maxMessageSizeStr
+                                + ". Using default value of "
                                 + TConfiguration.DEFAULT_MAX_MESSAGE_SIZE
-                                + ".");
+                                + " bytes.");
+
                 maxMessageSize = TConfiguration.DEFAULT_MAX_MESSAGE_SIZE;
             }
-        }
 
-        if (maxMessageSize <= 0) {
-
-            log.warn(
-                    "THRIFT_MAX_MESSAGE_SIZE is set to zero or less. Setting THRIFT_MAX_MESSAGE_SIZE to default value of "
+        } else {
+            log.info(
+                    "THRIFT_MAX_MESSAGE_SIZE environmental variable is not set. Setting THRIFT_MAX_MESSAGE_SIZE to default value of "
                             + TConfiguration.DEFAULT_MAX_MESSAGE_SIZE
                             + " bytes.");
             maxMessageSize = TConfiguration.DEFAULT_MAX_MESSAGE_SIZE;
         }
-        log.info("THRIFT_MAX_MESSAGE_SIZE is now set to " + maxMessageSize
-                + " bytes");
 
         // Parse MAX_FRAME_SIZE
-        if (maxFrameSizeStr == null) {
-            log.warn("THRIFT_MAX_FRAME_SIZE environmental variable not set.");
-        } else {
+        if (maxFrameSizeStr != null) {
             try {
                 maxFrameSize = Integer.parseInt(maxFrameSizeStr);
+
+                if (maxFrameSize > 0) {
+                    log.info("THRIFT_MAX_FRAME_SIZE is set to " + maxFrameSize
+                            + " bytes");
+                } else {
+                    log.info(
+                            "THRIFT_MAX_FRAME_SIZE is not set or set to an invalid value. Setting THRIFT_MAX_FRAME_SIZE to default value of "
+                                    + TConfiguration.DEFAULT_MAX_FRAME_SIZE
+                                    + " bytes.");
+                    maxFrameSize = TConfiguration.DEFAULT_MAX_FRAME_SIZE;
+                }
             } catch (NumberFormatException e) {
-                log.warn(
+                log.info(
                         "Could not parse value from THRIFT_MAX_FRAME_SIZE environmental variable. Value received was "
-                                + maxFrameSizeStr + ". "
-                                + "Setting THRIFT_MAX_FRAME_SIZE to the default value of "
-                                + TConfiguration.DEFAULT_MAX_FRAME_SIZE + ".");
+                                + maxFrameSizeStr + ". Using default value of "
+                                + TConfiguration.DEFAULT_MAX_FRAME_SIZE + " bytes.");
                 maxFrameSize = TConfiguration.DEFAULT_MAX_FRAME_SIZE;
             }
-        }
-
-        if (maxFrameSize <= 0) {
-            log.warn(
-                    "THRIFT_MAX_FRAME_SIZE is set to zero or less. Setting THRIFT_MAX_FRAME_SIZE to default value of "
-                            + TConfiguration.DEFAULT_MAX_FRAME_SIZE
-                            + " bytes.");
+        } else {
+            log.info(
+                    "THRIFT_MAX_FRAME_SIZE environmental variable is not set. Setting THRIFT_MAX_FRAME_SIZE to default value of "
+                            + TConfiguration.DEFAULT_MAX_FRAME_SIZE + " bytes.");
             maxFrameSize = TConfiguration.DEFAULT_MAX_FRAME_SIZE;
 
         }
-        log.info("THRIFT_MAX_FRAME_SIZE is now set to " + maxFrameSize
-                + " bytes");
 
         // Parse RECURSION_DEPTH
-        if (recursionDepthStr == null) {
-            log.warn("THRIFT_RECURSION_DEPTH environmental variable not set.");
-        } else {
+        if (recursionDepthStr != null) {
             try {
                 recursionDepth = Integer.parseInt(recursionDepthStr);
+                if (recursionDepth > 0) {
+                    log.info("THRIFT_RECURSION_DEPTH is set to "
+                            + recursionDepth);
+                } else {
+                    log.info(
+                            "THRIFT_RECURSION_DEPTH is not set or set to an invalid value. Setting THRIFT_RECURSION_DEPTH to default value of "
+                                    + TConfiguration.DEFAULT_RECURSION_DEPTH);
+                    recursionDepth = TConfiguration.DEFAULT_RECURSION_DEPTH;
+                }
             } catch (NumberFormatException e) {
-                log.warn(
+                log.info(
                         "Could not parse value from THRIFT_RECURSION_DEPTH environmental variable. Value received was "
-                                + recursionDepthStr + ". "
-                                + "Setting THRIFT_RECURSION_DEPTH to the default value of "
+                                + recursionDepthStr
+                                + ". Using default value of "
                                 + TConfiguration.DEFAULT_RECURSION_DEPTH + ".");
                 recursionDepth = TConfiguration.DEFAULT_RECURSION_DEPTH;
             }
-        }
-
-        if (recursionDepth <= 0) {
-            log.warn(
-                    "THRIFT_RECURSION_DEPTH is set to zero or less. Setting THRIFT_RECURSION_DEPTH to default value of "
+        } else {
+            log.info(
+                    "THRIFT_RECURSION_DEPTH environmental variable is not set. Setting THRIFT_RECURSION_DEPTH to default value of "
                             + TConfiguration.DEFAULT_RECURSION_DEPTH);
             recursionDepth = TConfiguration.DEFAULT_RECURSION_DEPTH;
-
         }
-        log.info("THRIFT_RECURSION_DEPTH is now set to " + recursionDepth);
 
         config = new TConfiguration(maxMessageSize, maxFrameSize,
                 recursionDepth);
